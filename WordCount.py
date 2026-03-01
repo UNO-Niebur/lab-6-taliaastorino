9297ccd9ff2f3bb8ca327ba42016b17a890e8db3
taliaastorino <tastorino@unomaha.edu>
2026-03-01 15:10:41 -0600

    Update WordCount.py
    Commit

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,29 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Talia Astorino
+#Date: 03/01/2026
+#Purpose: Calculate the total number of lines, words, and characters in a text file.
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
-  for line in textFile:
-    print(line)
-  
+  try:
+    textFile = open("gettysberg.txt", 'r')
 
+    lineCount = 0
+    wordCount = 0
+    charCount = 0
+    
+    for line in textFile:
+      lineCount += 1
+      wordCount += len(line.split())
+      charCount += len(line)
+
+    textFile.close()
+
+    print("Lines:", lineCount)
+    print("Words:", wordCount)
+    print("Characters:", charCount)
+
+  except FileNotFoundError:
+    print("File not found.")
+  
 if __name__ == '__main__':
   main()
