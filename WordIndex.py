09eecc69b64db949dec459bf2d5b0863b61b0913
taliaastorino <tastorino@unomaha.edu>
2026-03-01 15:21:54 -0600

    Update WordIndex.py
    commit

--- WordIndex.py
+++ WordIndex.py
@@ -1,19 +1,36 @@
 #WordIndex.py
-#Name:
-#Date:
-#Assignment:
+#Name: Talia Astorino
+#Date: 03/01/2026
+#Purpose: Create a word to line to number index from a text file.
 
 def main():
-  textFile = open("fish.txt", 'r')
+  try:
+    textFile = open("fish.txt", 'r')
   
-  words = {} #create an empty dictionary
-  
-  
-  print ("fish" in words) #is a word already in the dictionary?
-  words["fish"] = [2]     #add a list to the dictionary
-  print ("fish" in words) #is the word there now?
-  words["fish"].append(5) #add to an existing list
-  print(words)
+    words = {} #create an empty dictionary
+
+    lineNumber = 0
+
+    for line in textFile:
+      lineNumber += 1
+      lineWords = line.split()
+
+      for word in lineWords:
+        word = word.lower()
+
+        if word in words:
+          if lineNumber not in words[word]:
+            words[word].append(lineNumber)
+        else:
+          words[word] = [lineNumber]
+          
+    textFile.close()
+
+    for word in sortedIwords):
+      print(word, ":", words[word])
+
+  except FileNotFoundError:
+    print("File not found.")
 
 
 if __name__ == '__main__':
